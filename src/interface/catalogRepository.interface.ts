cf5b43f5a5f2adfdafa27bc71c0dc5b96e1c7d19
Alfred <102339353+fatmes2017@users.noreply.github.com>
2026-02-08 20:19:14 +0330

    first commit

---
+++ src/interface/catalogRepository.interface.ts
@@ -0,0 +1,11 @@
+import { Product } from "../models/product.model";
+
+
+export interface ICatalogRepository
+{
+create(data:Product):Promise<Product>;
+update(data:Product):Promise<Product>;
+delete(id:any):any;
+find():Promise<Product[]>;
+findOne(id:number):Promise<Product>;
+};
