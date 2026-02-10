622dbf41eaf1640075c22b1334a892ad2a724d94
Alfred <102339353+fatmes2017@users.noreply.github.com>
2026-02-10 13:05:45 +0330

    debug testt

--- src/service/__test__/catalog.service.test.ts
+++ src/service/__test__/catalog.service.test.ts
@@ -1,65 +1,67 @@
-//import { faker } from "@faker-js/faker";
-import { ICatalogRepository } from "../../interface/catalogRepository.interface";
-import { Product } from "../../models/product.model";
 import { MockCatalogRepository } from "../../repository/mockCatalog.repository";
 import { CatalogService } from "../catalog.service";
+import { faker } from '@faker-js/faker';
+
 
 const mockProduct = (rest: any) => {
   return {
-    name: "onion", //faker.commerce.productName(),
+    name: "piyaz",
     description: "vegetables", //faker.commerce.productDescription(),
     stock: 20, // faker.number.int({min:10,max:100}),
     ...rest,
   };
 };
 
+//create this function as beforeEach
+const prepareRepo = (): MockCatalogRepository => {
+  return new MockCatalogRepository();
+};
+//create this function as afterEach
+const purgeRepo = (repository: MockCatalogRepository) => {
+  repository = {} as MockCatalogRepository;
+};
+
 describe("catalogService", () => {
-  let repository: ICatalogRepository;
+  //let repository = new MockCatalogRepository();
+  // beforeEach(() => {
+  //   let repository = new MockCatalogRepository();
+  // });
+  let repository: MockCatalogRepository = prepareRepo();
 
-  beforeEach(() => {
-    let repository = new MockCatalogRepository();
-  });
+  // afterEach(() => {
+  //   repository = {} as MockCatalogRepository;
+  // });
 
-  afterEach(() => {
-    repository = {} as MockCatalogRepository;
-  });
-
-  describe("createProduct", () => {
+  describe("CreateProduct", () => {
     test("should create product", async () => {
       const service = new CatalogService(repository);
       const reqBody = mockProduct({
         price: 10, //+faker.commerce.price(),
       });
-      const result = await service?.createProduct(reqBody);
+
+      const result = await service.createProduct(reqBody);
 
       expect(result).toMatchObject({
-        id: expect.any(Number),
+        // id: expect.any(Number),
         name: expect.any(String),
         description: expect.any(String),
         price: expect.any(Number),
         stock: expect.any(Number),
       });
+      //  expect(result.name).toBe("farzad");
     });
 
+    // test("should throw error with product already exist", async () => {
+    //   const service = new CatalogService(repository);
+    //   const reqBody = mockProduct({
+    //     price: 10, //+faker.commerce.price(),
+    //   });
+    //   jest.spyOn(repository, "create").mockImplementationOnce(() => Promise.resolve({} as Product));
+    //   await expect(service.createProduct(reqBody)).rejects.toThrow(
+    //     "unable to create product",
+    //   );
+    // });
 
-    test("should throw error with product already exist", async () => {
-      const service = new CatalogService(repository);
-      const reqBody = mockProduct({
-        price: 10, //+faker.commerce.price(),
-      });
-      jest.spyOn(repository, "create").mockImplementationOnce(() => Promise.resolve({} as Product));
-      await expect(service.createProduct(reqBody)).rejects.toThrow(
-        "unable to create product",
-      );
-    });
-
-
-
-
-
+    purgeRepo(repository);
   });
-
-
-
-
 });
